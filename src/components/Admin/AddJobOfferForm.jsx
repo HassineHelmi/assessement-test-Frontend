ebeb3e1c4b4dfedbb43c0fb47d82417b60711ae4
HassineHelmi <helmi.hassine1@gmail.com>
2023-09-12 04:28:53 +0100

    pagination  delete and toggle option added

--- src/components/Admin/AddJobOfferForm.jsx
+++ src/components/Admin/AddJobOfferForm.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+
 const AddJobOfferForm = () => {
   const [formData, setFormData] = useState({
     jobTitle: "",
     description: "",
     applicationDeadline: "",
   });
+  const [message, setMessage] = useState(""); // State to store the success message
 
   const navigate = useNavigate();
 
@@ -16,7 +18,7 @@ const AddJobOfferForm = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
+
     try {
       const response = await fetch("http://localhost:3000/job-offers", {
         method: "POST",
@@ -26,24 +28,31 @@ const AddJobOfferForm = () => {
         },
         body: JSON.stringify(formData),
       });
-  
+
       if (response.ok) {
         // Handle success
+        setMessage("Job offer created successfully!");
+        setFormData({ // Clear the form fields
+          jobTitle: "",
+          description: "",
+          applicationDeadline: "",
+        });
         navigate("/dashboard");
       } else {
         // Handle error response
         const errorData = await response.json();
-        console.error("Error creating job offer:", errorData);
+        setMessage(`Error creating job offer: ${errorData.message}`);
       }
     } catch (error) {
       // Handle network or other errors
-      console.error("Error creating job offer", error);
+      setMessage("Error creating job offer. Please try again later.");
     }
   };
 
   return (
     <div className="p-6 bg-white rounded-lg shadow-lg">
-      <h2 className="text-2xl font-semibold mb-4">Add New Job Offer :</h2>
+      <h2 className="text-2xl font-semibold mb-4">Add New Job Offer:</h2>
+      {message && <p className="text-green-600">{message}</p>}
       <form onSubmit={handleSubmit}>
         <div className="mb-4">
           <label className="block text-gray-700 font-semibold">Job Title</label>
@@ -51,7 +60,7 @@ const AddJobOfferForm = () => {
             className="form-input mt-1 block w-full p-2 rounded-lg border border-gray-300 focus:outline-none focus:ring focus:border-teal-500"
             type="text"
             name="jobTitle"
-            value={formData.jobTitle} // Set the controlled value
+            value={formData.jobTitle}
             onChange={handleChange}
             required
           />
@@ -62,7 +71,7 @@ const AddJobOfferForm = () => {
             className="form-textarea mt-1 block w-full p-2 rounded-lg border border-gray-300 focus:outline-none focus:ring focus:border-teal-500"
             rows="4"
             name="description"
-            value={formData.description} // Set the controlled value
+            value={formData.description}
             onChange={handleChange}
             required
           />
@@ -73,7 +82,7 @@ const AddJobOfferForm = () => {
             className="form-input mt-1 block w-full p-2 rounded-lg border border-gray-300 focus:outline-none focus:ring focus:border-teal-500"
             type="date"
             name="applicationDeadline"
-            value={formData.applicationDeadline} // Set the controlled value
+            value={formData.applicationDeadline}
             onChange={handleChange}
             required
           />
--- src/components/Admin/ManageJobOffers.jsx
+++ src/components/Admin/ManageJobOffers.jsx
@@ -1,13 +1,19 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Pagination from "./Pagination";
 
 const ManageJobOffers = () => {
   const [jobOffers, setJobOffers] = useState([]);
   const [error, setError] = useState(null);
-
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editData, setEditData] = useState({});
+  const [formData, setFormData] = useState({
+    jobTitle: "",
+    description: "",
+    deadline: "",
+  });
 
   useEffect(() => {
     const accessToken = localStorage.getItem("access_token");
@@ -39,11 +45,75 @@ const ManageJobOffers = () => {
     };
 
     fetchJobOffers(currentPage); // Use the currentPage state as the page parameter
-  }, [currentPage]); // Include currentPage in the dependency array
+  }, [currentPage]);
 
   const handlePageChange = (currentPage) => {
     setCurrentPage(currentPage);
   };
+
+ 
+
+ 
+
+  const toggleJobStatus = async (jobOffer) => {
+    const accessToken = localStorage.getItem("access_token");
+    const status = jobOffer.open ? "close" : "open"; // Determine the new status
+
+    try {
+      const response = await fetch(
+        `http://localhost:3000/job-offers/${jobOffer.id}/${status}`,
+        {
+          method: "PUT",
+          headers: {
+            Authorization: "Bearer " + accessToken,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      // Update the jobOffers state with the updated job offer
+      setJobOffers((prevJobOffers) =>
+        prevJobOffers.map((offer) =>
+          offer.id === jobOffer.id ? { ...offer, open: !offer.open } : offer
+        )
+      );
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  const deleteJobOffer = async (jobOffer) => {
+    const accessToken = localStorage.getItem("access_token");
+    
+
+    try {
+      const response = await fetch(
+        `http://localhost:3000/job-offers/${jobOffer.id}`,
+        {
+          method: "DELETE",
+          headers: {
+            Authorization: "Bearer " + accessToken,
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      // Remove the deleted job offer from the jobOffers state
+      setJobOffers((prevJobOffers) =>
+        prevJobOffers.filter((offer) => offer.id !== jobOffer.id)
+      );
+    } catch (error) {
+      setError(error);
+    }
+  };
+
   return (
     <div className="bg-gray-800 h-100vw p-10">
       <div className="flex justify-between items-center mb-6">
@@ -64,7 +134,33 @@ const ManageJobOffers = () => {
               Deadline:{" "}
               {new Date(offer.applicationDeadline).toLocaleDateString()}
             </p>
-            <div className="mt-2 flex items-center"></div>
+            <div className="mt-2 flex items-center">
+              {/* Button to toggle job status */}
+              <button
+                onClick={() => toggleJobStatus(offer)}
+                className={`${
+                  offer.open ? "bg-green-500" : "bg-red-500"
+                } text-white py-2 px-4 rounded-lg hover:bg-teal-600`}
+              >
+                {offer.open ? "Job Open" : "Job Closed"}
+              </button>
+              {/* Button to delete job offer */}
+              {offer.open ? (
+                <button
+                  disabled
+                  className="bg-gray-500 text-white py-2 px-4 rounded-lg ml-2"
+                >
+                  Delete
+                </button>
+              ) : (
+                <button
+                  onClick={() => deleteJobOffer(offer)}
+                  className="bg-red-500 text-white py-2 px-4 rounded-lg ml-2 hover:bg-red-600"
+                >
+                  Delete
+                </button>
+              )}
+            </div>
           </li>
         ))}
       </ul>
@@ -73,4 +169,5 @@ const ManageJobOffers = () => {
     </div>
   );
 };
+
 export default ManageJobOffers;
